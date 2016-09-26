dccd5e098543a4ddb83e1fcd8370998de6695b0a
Svetozare <svetozare.milev@gmail.com>
2016-09-26 09:52:41 -0400

    GIT Init

---
+++ src/helloclass/HelloClass.java
@@ -0,0 +1,13 @@
+
+package helloclass;
+
+public class HelloClass {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+      System.out.println("Hello Class");
+    }
+    
+}
