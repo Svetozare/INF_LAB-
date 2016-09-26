9090ce7dcab65afd0f7c68de4b8d9547abeb8491
nabourama <seya00@gmail.com>
2016-09-26 10:37:52 -0400

    Ajout  Bonjour

--- src/helloclass/HelloClass.java
+++ src/helloclass/HelloClass.java
@@ -9,6 +9,7 @@ public class HelloClass {
     public static void main(String[] args) {
       System.out.println("Hello Class");
       System.out.println("GITHUB Lab test");
+      System.out.println("Boujour !");
     }
     
 }
